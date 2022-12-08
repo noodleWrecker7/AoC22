dc7eb9b870636ad7809d4fb512e31c0e77bdae38
Adam Hodgkinson <hodgkinson.adam@gmail.com>
2022-12-08 15:34:58 +0000

    day4 part 1 done

--- src/dev/adam/Day4.java
+++ src/dev/adam/Day4.java
@@ -7,9 +7,9 @@ public class Day4
 {
     public static void main(String[] args)
     {
-        D4Part1 p1 = new D4Part1(Util.getFile("day2.txt"));
+        D4Part1 p1 = new D4Part1(Util.getFile("day4.txt"));
         System.out.println(p1.solve());
-        D4Part2 p2 = new D4Part2(Util.getFile("day2.txt"));
+        D4Part2 p2 = new D4Part2(Util.getFile("day4.txt"));
         System.out.println(p2.solve());
     }
 
@@ -25,9 +25,42 @@ class D4Part1
         this.fr = fr;
     }
 
+    class Range
+    {
+        int start;
+        int end;
+
+        public Range(int start, int end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+    }
+
     public int solve()
     {
-        return score;
+        int overlaps = 0;
+        String line;
+        try
+        {
+            while ((line = fr.readLine()) != null)
+            {
+                String[] parts = line.split(",");
+                // I don't like how obtuse this is, but I don't care
+                String[][] ranges = new String[][]{parts[0].split("-"), parts[1].split("-")};
+                Range elf1 = new Range(Integer.parseInt(ranges[0][0]), Integer.parseInt(ranges[0][1]));
+                Range elf2 = new Range(Integer.parseInt(ranges[1][0]), Integer.parseInt(ranges[1][1]));
+
+                if (elf1.start >= elf2.start && elf1.end <= elf2.end || elf1.start <= elf2.start && elf1.end >= elf2.end)
+                {
+                    overlaps++;
+                }
+            }
+        } catch (IOException e)
+        {
+            System.out.println("Uh Oh!");
+        }
+        return overlaps;
     }
 }
 
@@ -43,6 +76,6 @@ class D4Part2
 
     public int solve()
     {
-        return score;
+        return 0;
     }
 }
